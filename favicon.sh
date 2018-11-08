0159fb1aae10e0728f7f8712d4ef4baa3d478e24
Ruslan Ibragimov <ruslan@ibragimov.by>
2018-11-08 20:31:18 +0300

    Generate two sets of icons, and favicon

--- favicon.sh
+++ favicon.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-file="${1}";
-sizes="16 32 57 72 76 114 120 144 152 180 196";
+small_file="${1}";
+small_sizes="16 32 57 72 76";
 
-for size in ${sizes}; do
-  convert "${file}" -resize "${size}x${size}" "favicon-${size}.png";
+echo "Generating different pngs"
+
+for size in ${small_sizes}; do
+  convert "${small_file}" -resize "${size}x${size}" "favicon-${size}.png";
 done
+
+big_file="${2}";
+big_sizes="114 120 144 152 180 196";
+
+for size in ${big_sizes}; do
+  convert "${big_file}" -resize "${size}x${size}" "favicon-${size}.png";
+done
+
+echo "Generating favicon.ico"
+
+convert -background transparent "${small_file}" -define icon:auto-resize=16,24,32,48,64 "favicon.ico"
